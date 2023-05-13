bcbee6e5530e57c8d00bec401cbf90ea9d5b47a1
H1B0B0 <hibosmurf@gmail.com>
2023-05-13 20:28:55 +0200

    Dialogue box added for personnal proxy import (Only HTTP for this time)

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ import tkinter as tk
 from tkinter import ttk
 from random import shuffle
 from threading import Thread
+from tkinter import filedialog
 from streamlink import Streamlink
 from fake_useragent import UserAgent
 
 channel_url = ""
 processes = []
 
-all_proxies = []
 nb_of_proxies = 0
 
 # Session creating for request
@@ -31,22 +31,27 @@ session.set_option("http-headers", {
 })
 
 class ViewerBot:
-    def __init__(self, nb_of_threads, channel_name, label, stop=False):
+    def __init__(self, nb_of_threads, channel_name, label,proxylist, stop=False):
         self.nb_of_threads = nb_of_threads
         self.channel_name = channel_name
         self.nb_requests = 0
         self.nb_requests_label = label
         self.stop_event = stop
+        self.proxylist = proxylist
+        self.all_proxies = []
 
     def get_proxies(self):
-        try:
-            response = requests.get("https://api.proxyscrape.com/v2/?request=displayproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all")
-            if response.status_code == 200:
-                lines = response.text.split("\n")
-                lines = [line.strip() for line in lines if line.strip()]
-                return lines
-        except:
-            pass
+        if self.proxylist == None : 
+            try:
+                response = requests.get("https://api.proxyscrape.com/v2/?request=displayproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all")
+                if response.status_code == 200:
+                    lines = response.text.split("\n")
+                    lines = [line.strip() for line in lines if line.strip()]
+                    return lines
+            except:
+                pass
+        else :
+            return self.proxylist
 
     def get_url(self):
         url = ""
@@ -63,9 +68,8 @@ class ViewerBot:
 
     def open_url(self, proxy_data):
         try:
-            global all_proxies
             headers = {'User-Agent': ua.random}
-            current_index = all_proxies.index(proxy_data)
+            current_index = self.all_proxies.index(proxy_data)
 
             if proxy_data['url'] == "":
                 proxy_data['url'] = self.get_url()
@@ -78,7 +82,7 @@ class ViewerBot:
                     self.nb_requests += 1
                     self.nb_requests_label.config(text=f"Number of requests: {self.nb_requests}")
                     proxy_data['time'] = time.time()
-                    all_proxies[current_index] = proxy_data
+                    self.all_proxies[current_index] = proxy_data
             except:
                 pass
         except (KeyboardInterrupt):
@@ -97,10 +101,10 @@ class ViewerBot:
             elapsed_seconds = (datetime.datetime.now() - start).total_seconds()
 
             for p in proxies:
-                all_proxies.append({'proxy': p, 'time': time.time(), 'url': ""})
+                self.all_proxies.append({'proxy': p, 'time': time.time(), 'url': ""})
 
             for i in range(0, int(self.nb_of_threads)):
-                self.threaded = Thread(target=self.open_url, args=(all_proxies[random.randrange(len(all_proxies))],))
+                self.threaded = Thread(target=self.open_url, args=(self.all_proxies[random.randrange(len(self.all_proxies))],))
                 self.threaded.daemon = True  # This thread dies when main thread (only non-daemon thread) exits.
                 self.threaded.start()
 
@@ -109,21 +113,21 @@ class ViewerBot:
                 proxies = self.get_proxies()
                 elapsed_seconds = 0  # reset elapsed time
 
-            shuffle(all_proxies)
+            shuffle(self.all_proxies)
 
         
 class ViewerBotGUI:
     def __init__(self):
         self.window = tk.Tk()
         self.window.title("ViewerBot")
-        current_dir = os.path.dirname(os.path.abspath(__file__))
+        self.current_dir = os.path.dirname(os.path.abspath(__file__))
         now = datetime.datetime.now()
         hour = now.hour
         if hour >= 19: 
             sv_ttk.set_theme("dark")
         else:
             sv_ttk.set_theme("light")
-        self.window.wm_iconbitmap(f"{current_dir}/R.ico")
+        self.window.wm_iconbitmap(f"{self.current_dir}/R.ico")
         self.nb_requests_label = ttk.Label(self.window, text="Number of requests: 0")
         self.nb_requests_label.grid(column=0, row=5, columnspan=2, padx=10, pady=10)
         self.nb_requests = 0
@@ -162,21 +166,19 @@ class ViewerBotGUI:
         # Variables for status and threads
         self.status = "Stopped"
         self.threads = []
+
+        self.show_dialog()
         
         self.window.mainloop()
         
     def start_bot(self):
         if self.status == "Stopped":
-            global max_nb_of_threads
             nb_of_threads = self.nb_threads_entry.get()
             self.channel_name = self.channel_name_entry.get()
-            self.bot = ViewerBot(nb_of_threads, self.channel_name, self.nb_requests_label , )
+            self.bot = ViewerBot(nb_of_threads, self.channel_name, self.nb_requests_label , self.proxylist)
             self.thread = Thread(target=self.bot.main)
             self.thread.daemon = True
             self.thread.start()
-            max_nb_of_threads = nb_of_threads
-            max_nb_of_threads = int(max_nb_of_threads)
-            max_nb_of_threads = max_nb_of_threads*10
             # Change status and disable/enable buttons
             self.status = "Running"
             self.nb_threads_entry.config(state="disabled")
@@ -207,6 +209,58 @@ class ViewerBotGUI:
             status_label.grid(column=0, row=4, columnspan=2, padx=10, pady=10)
 
             self.bot.stop()
+
+    def show_dialog(self):
+
+        self.proxylist = []
+
+        # create new window for the parameters
+        self.dialog = tk.Toplevel(self.window)
+        self.dialog.title("Parameters")
+        self.dialog.iconbitmap(f"{self.current_dir}/R.ico")
+
+        # Button for import proxy list
+        open_file_button = ttk.Button(self.dialog, text="import your proxy list (only http)")
+        open_file_button.grid(column=1, row=1, padx=10, pady=10)
+        open_file_button.config(command=self.on_open_file) 
+
+        scraped_button = ttk.Button(self.dialog, text="scraped automatically proxy")
+        scraped_button.grid(column=1, row=2, padx=10, pady=10)
+        scraped_button.config(command=self.scraped_proxy) 
+
+
+        self.dialog.protocol("WM_DELETE_WINDOW", self.scraped_proxy)
+        # center the parameters window about the self.window window
+        self.dialog.update_idletasks()
+        width = self.dialog.winfo_width()
+        height = self.dialog.winfo_height()
+        x = self.window.winfo_x() + (self.window.winfo_width() - width) // 2
+        y = self.window.winfo_y() + (self.window.winfo_height() - height) // 2
+        self.dialog.geometry(f"{width}x{height}+{x}+{y}")
+        self.window.wait_window(self.dialog)
+        
+    def scraped_proxy(self):
+        self.proxylist = None
+
+        self.dialog.destroy()
+        
+        
+    # validation button
+    def on_open_file(self):
+
+        
+        file_path = filedialog.askopenfilenames(filetypes=[("Text Files", "*.txt")])
+
+
+        for path in file_path:
+            with open(path, 'r') as f:
+                for line in f:
+                    self.proxylist.append(line.strip())
+        
+        # close the parameters window
+        self.dialog.destroy()
+
+
                       
 
 if __name__ == '__main__':
