c9ba570e5dfe16b35f5fd69cfa6d87c9d27a495c
etienne <etienne.mentrel@scafe.io>
2024-05-30 22:21:39 +0200

    Refactor login.py for Discord OAuth2 login functionality

--- src/twitchbot/main.py
+++ src/twitchbot/main.py
@@ -1,17 +1,36 @@
+import os
+import requests
+import webbrowser
 import customtkinter
 import platform
 from pathlib import Path
 from threading import Thread
 from tkinter import messagebox
+from dotenv import load_dotenv
 from tkinter import filedialog
 from viewer_bot import ViewerBot
+from flask import Flask, request, redirect
 
 current_path = Path(__file__).resolve().parent
 ICON = current_path/"interface_assets"/"R.ico"
 THEME = current_path/"interface_assets"/"purple.json"
 
 SLIDER_MIN = 1000
-SLIDER_MAX = 20000
+SLIDER_MAX = 10000
+
+app = Flask(__name__)
+current_path = Path(__file__).resolve().parent
+
+# Load .env file
+load_dotenv()
+
+# Get the credentials from .env
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+REDIRECT_URI = os.getenv('REDIRECT_URI')
+SERVER_ID = os.getenv('SERVER_ID')
+ROLE_ID = os.getenv('ROLE_ID')
+BOT_TOKEN = os.getenv('BOT_TOKEN')
 
 class ViewerBotGUI(customtkinter.CTk):
     def __init__(self):
@@ -26,76 +45,193 @@ class ViewerBotGUI(customtkinter.CTk):
         self.nb_of_proxies = 0
         self.nb_of_proxies_alive = 0
         self.message_gived = False
-        
-        # Label for number of threads
-        nb_threads_label = customtkinter.CTkLabel(self, text="Number of threads:")
-        nb_threads_label.grid(column=0, row=0, padx=10, pady=10)
-        
-        # Entry for number of threads
-        self.nb_threads_entry = customtkinter.CTkEntry(self)
-        self.nb_threads_entry.grid(column=1, row=0, padx=10, pady=10)
-        
-        # Label for Twitch channel name
-        channel_name_label = customtkinter.CTkLabel(self, text="Twitch channel name:")
-        channel_name_label.grid(column=0, row=1, padx=10, pady=10)
-        
-        # Entry for Twitch channel name
-        self.channel_name_entry = customtkinter.CTkEntry(self)
-        self.channel_name_entry.grid(column=1, row=1, padx=10, pady=10)
-
-        # Label for proxy type
-        proxy_type = customtkinter.CTkLabel(self, text="Proxy type")
-        proxy_type.grid(column=0, row=2, columnspan=2, padx=10, pady=0)
-
-        # select proxy type
-        self.segemented_button_var = customtkinter.StringVar(value="http")
-        self.segemented_button = customtkinter.CTkSegmentedButton(self, values=["http", "socks4", "socks5", "all"], variable=self.segemented_button_var)
-        self.segemented_button.grid(column=0, row=3, columnspan=2, padx=10, pady=5)
-
-        self.slider = customtkinter.CTkSlider(self, from_=SLIDER_MIN, to=SLIDER_MAX, command=self.slider_event)
-        self.slider.set(SLIDER_MAX)
-        self.slider.grid(column=0, row=5, columnspan=2, padx=10, pady=0)
-
-        # Label for timeout
-        self.timeout = customtkinter.CTkLabel(self, text=f"timeout: {int(self.slider.get())}")
-        self.timeout.grid(column=0, row=4, columnspan=2, padx=10, pady=0)
-        
-        # Button to start the bot
-        start_button = customtkinter.CTkButton(self, text="Start bot")
-        start_button.grid(column=0, row=6, padx=10, pady=10)
-        start_button.configure(command=self.start_bot)
-        
-        # Button to stop the bot
-        stop_button = customtkinter.CTkButton(self, text="Stop", state="normal")
-        stop_button.grid(column=1, row=6, padx=10, pady=10)
-        stop_button.configure(command=self.stop_bot)
-        
-        self.nb_requests_label = customtkinter.CTkLabel(self, text="Number of requests: 0")
-        self.nb_requests_label.grid(column=0, row=7, columnspan=2, padx=10, pady=2)
-        # Label for status
-        self.status_label = customtkinter.CTkLabel(self, text="Status: Stopped")
-        self.status_label.grid(column=0, row=8, columnspan=2, padx=10, pady=2)
-
-        # Label for the proxy states
-        self.proxies_label = customtkinter.CTkLabel(self, text="proxy states")
-        self.proxies_label.grid(column=0, columnspan=2, row=9, padx=10, pady=2)
-
-        # Label for the proxy states
-        self.total_label = customtkinter.CTkLabel(self, text=f"Total:{self.nb_of_proxies}")
-        self.total_label.grid(column=0, row=10, padx=10, pady=2, sticky="w")
-
-        # Label for the proxy states
-        self.alive_label = customtkinter.CTkLabel(self, text=f"Alive:{self.nb_of_proxies_alive}")
-        self.alive_label.grid(column=1, row=10, padx=10, pady=2, sticky="e")
-
-        # Label
-        self.name_label = customtkinter.CTkLabel(self, text="Coded by HIBOBO")
-        self.name_label.grid(column=0, columnspan=2, row=11, padx=10, pady=2)
-        
-        # Variables for status and threads
-        self.status = "Stopped"
+        self.logged_in = False
+        self.browser = None
+        self.nb_threads_entry = None
+        self.channel_name_entry = None
+        self.status_label = None
+        self.total_label = None
+        self.alive_label = None
+        self.name_label = None
+        self.segemented_button_var = None
+        self.timeout = None
+        self.nb_requests_label = None
+        self.proxies_label = None
         self.threads = []
-        self.show_dialog()
+        self.segemented_button = None
+        
+        @app.route('/')
+        def index():
+            url = f'https://discord.com/api/oauth2/authorize?client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&response_type=code&scope=identify+role_connections.write+guilds+guilds.members.read'
+            return redirect(url, code=302)
+
+        @app.route('/callback')
+        def callback():
+            code = request.args.get('code')
+            data = {
+                'client_id': CLIENT_ID,
+                'client_secret': CLIENT_SECRET,
+                'grant_type': 'authorization_code',
+                'code': code,
+                'redirect_uri': REDIRECT_URI,
+                'scope': 'identify guilds'
+            }
+            headers = {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+            response = requests.post('https://discord.com/api/oauth2/token', data=data, headers=headers)
+            response.raise_for_status()
+            token = response.json()['access_token']
+            print(f"Access token: {token}")
+            headers = {
+                'Authorization': f'Bearer {token}'
+            }
+            response = requests.get('https://discord.com/api/users/@me', headers=headers)
+            response.raise_for_status()
+            user_id = response.json()['id']  # Get the user's ID
+            response = requests.get('https://discord.com/api/users/@me/guilds', headers=headers)
+            response.raise_for_status()
+            guilds = response.json()
+            guild_ids = [guild['id'] for guild in guilds]
+            if SERVER_ID in guild_ids:
+                headers = {
+                        'Authorization': f'Bot {BOT_TOKEN}'  # Use Bot token here
+                    }
+                response = requests.get(f'https://discord.com/api/guilds/{SERVER_ID}/members/{user_id}', headers=headers)  # Use the user's ID here
+                response.raise_for_status()
+                roles = response.json()['roles']
+                if ROLE_ID in roles:
+                    self.label.destroy()
+                    self.login_button.destroy()
+                    # Label for number of threads
+                    nb_threads_label = customtkinter.CTkLabel(self, text="Number of threads:")
+                    nb_threads_label.grid(column=0, row=0, padx=10, pady=10)
+                    
+                    # Entry for number of threads
+                    self.nb_threads_entry = customtkinter.CTkEntry(self)
+                    self.nb_threads_entry.grid(column=1, row=0, padx=10, pady=10)
+                    
+                    # Label for Twitch channel name
+                    channel_name_label = customtkinter.CTkLabel(self, text="Twitch channel name:")
+                    channel_name_label.grid(column=0, row=1, padx=10, pady=10)
+                    
+                    # Entry for Twitch channel name
+                    self.channel_name_entry = customtkinter.CTkEntry(self)
+                    self.channel_name_entry.grid(column=1, row=1, padx=10, pady=10)
+
+                    # Label for proxy type
+                    proxy_type = customtkinter.CTkLabel(self, text="Proxy type")
+                    proxy_type.grid(column=0, row=2, columnspan=2, padx=10, pady=0)
+
+                    # select proxy type
+                    self.segemented_button_var = customtkinter.StringVar(value="http")
+                    self.segemented_button = customtkinter.CTkSegmentedButton(self, values=["http", "socks4", "socks5", "all"], variable=self.segemented_button_var)
+                    self.segemented_button.grid(column=0, row=3, columnspan=2, padx=10, pady=5)
+
+                    self.slider = customtkinter.CTkSlider(self, from_=SLIDER_MIN, to=SLIDER_MAX, command=self.slider_event)
+                    self.slider.set(SLIDER_MAX)
+                    self.slider.grid(column=0, row=5, columnspan=2, padx=10, pady=0)
+
+                    # Label for timeout
+                    self.timeout = customtkinter.CTkLabel(self, text=f"timeout: {int(self.slider.get())}")
+                    self.timeout.grid(column=0, row=4, columnspan=2, padx=10, pady=0)
+                    
+                    # Button to start the bot
+                    start_button = customtkinter.CTkButton(self, text="Start bot")
+                    start_button.grid(column=0, row=6, padx=10, pady=10)
+                    start_button.configure(command=self.start_bot)
+                    
+                    # Button to stop the bot
+                    stop_button = customtkinter.CTkButton(self, text="Stop", state="normal")
+                    stop_button.grid(column=1, row=6, padx=10, pady=10)
+                    stop_button.configure(command=self.stop_bot)
+                    
+                    self.nb_requests_label = customtkinter.CTkLabel(self, text="Number of requests: 0")
+                    self.nb_requests_label.grid(column=0, row=7, columnspan=2, padx=10, pady=2)
+                    # Label for status
+                    self.status_label = customtkinter.CTkLabel(self, text="Status: Stopped")
+                    self.status_label.grid(column=0, row=8, columnspan=2, padx=10, pady=2)
+
+                    # Label for the proxy states
+                    self.proxies_label = customtkinter.CTkLabel(self, text="proxy states")
+                    self.proxies_label.grid(column=0, columnspan=2, row=9, padx=10, pady=2)
+
+                    # Label for the proxy states
+                    self.total_label = customtkinter.CTkLabel(self, text=f"Total:{self.nb_of_proxies}")
+                    self.total_label.grid(column=0, row=10, padx=10, pady=2, sticky="w")
+
+                    # Label for the proxy states
+                    self.alive_label = customtkinter.CTkLabel(self, text=f"Alive:{self.nb_of_proxies_alive}")
+                    self.alive_label.grid(column=1, row=10, padx=10, pady=2, sticky="e")
+
+                    # Label
+                    self.name_label = customtkinter.CTkLabel(self, text="Coded by HIBOBO")
+                    self.name_label.grid(column=0, columnspan=2, row=11, padx=10, pady=2)
+                    
+                    # Variables for status and threads
+                    self.status = "Stopped"
+                    self.show_dialog()
+                    return """
+                        <html>
+                            <head>
+                                <style>
+                                    body {
+                                        font-family: Arial, sans-serif;
+                                        background-color: #f0f0f0;
+                                        color: #333;
+                                        padding: 30px;
+                                    }
+                                    .message {
+                                        font-size: 24px;
+                                        font-weight: bold;
+                                        color: #008000;  # Green text
+                                    }
+                                </style>
+                            </head>
+                            <body>
+                                <div class="message">You are logged in</div>
+                            </body>
+                        </html>
+                        """
+                else:
+                    dialog = customtkinter.CTkInputDialog()
+                    dialog.title("Subscribe to the bot")
+                    label = customtkinter.CTkLabel(dialog, text="You are in the server, but you are not subscribed.\n Please subscribe to access the bot.\n If you are already subscribed, please contact the owner.\n")
+                    label.pack(pady=10, padx=10)
+                    link = customtkinter.CTkButton(dialog, text="Subscribe", command=open_link)
+                    link.pack(pady=10, padx=10)
+                    dialog.mainloop()
+            else:
+                dialog = customtkinter.CTkInputDialog()
+                dialog.title("Join the server")
+                label = customtkinter.CTkLabel(dialog, text="You are not in the server.\n Please join the server to access the bot.\n If you are already in the server, please contact the owner.\n")
+                label.pack(pady=10, padx=10)
+                link = customtkinter.CTkButton(dialog, text="Join", command=open_link_discord)
+                link.pack(pady=10, padx=10)
+                dialog.mainloop()
+
+        def run_flask():
+            app.run(debug=True, use_reloader=False)
+
+        def open_browser():
+            self.browser = webbrowser.open('http://localhost:5000')
+
+        def open_link():
+            self.browser = webbrowser.open('https://www.patreon.com/hibo/membership')
+
+        def open_link_discord():
+            self.browser = webbrowser.open('https://discord.gg/EDQ8ayhjDp')
+
+        if __name__ == '__main__':
+            flask_thread = Thread(target=run_flask)
+            flask_thread.daemon = True
+            flask_thread.start()
+
+            self.label = customtkinter.CTkLabel(self, text="Please login to access the bot")
+            self.label.pack(pady=10, padx=10)
+            self.login_button = customtkinter.CTkButton(self, text="Login", command=open_browser)
+            self.login_button.pack(pady=10, padx=10)
+                
 
     def slider_event(self, value):
         self.timeout.configure(text=f"timeout: {int(self.slider.get())}")
@@ -199,3 +335,7 @@ class ViewerBotGUI(customtkinter.CTk):
             messagebox.showwarning(title="WARNING", message="No proxy imported, the proxy list is empty. Proxies gonna be scraped.")
         # close the parameters window
         self.dialog.destroy()                 
+
+if __name__ == "__main__":
+    app = ViewerBotGUI()
+    app.mainloop()
